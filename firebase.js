a51ab787d6f3e86cb8f02ca1843f28c97e0a7a8f
Colecochran <timothycole2001@yahoo.com>
2026-06-04 11:39:34 -0700

    test

--- firebase.js
+++ firebase.js
@@ -1,10 +1,24 @@
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
+    const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+
+    if (!serviceAccount) {
+        console.error('[Firebase] FIREBASE_SERVICE_ACCOUNT env var is not set.');
+        console.error('[Firebase] Add it to your .env file or Render environment variables.');
+        process.exit(1);
+    }
+
+    let cert;
+    try {
+        cert = JSON.parse(serviceAccount);
+    } catch (e) {
+        console.error('[Firebase] FIREBASE_SERVICE_ACCOUNT is not valid JSON:', e.message);
+        process.exit(1);
+    }
+
     admin.initializeApp({
-        credential: admin.credential.cert(
-            JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-        )
+        credential: admin.credential.cert(cert)
     });
 }
 
